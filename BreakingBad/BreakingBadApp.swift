b67075fbe65cb40638e0e98af36930be10bd68be
Wandeep Basra <wandeep@wandeep.com>
2022-11-21 10:59:54 +0000

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Wandeep Basra on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
